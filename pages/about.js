37c2480fb16c06875722b37ca523b0faa448575b
SankhaChak <anonproductionspro@gmail.com>
2021-06-29 13:05:23 +0530

    ✅ feat: added starting pages

---
+++ pages/about.js
@@ -0,0 +1,13 @@
+import Layout from "../components/Layout";
+
+export default function AboutPage() {
+  return (
+    <Layout title="About DJ Events">
+      <h1>About</h1>
+      <p>
+        This is an app to find the latest musical happenings around your city
+      </p>
+      <p>Version: 1.0.1</p>
+    </Layout>
+  );
+}
---
+++ pages/events/[slug].js
@@ -0,0 +1,9 @@
+import Layout from "../../components/Layout";
+
+export default function EventDetailPage() {
+  return (
+    <Layout>
+      <h1>My Event</h1>
+    </Layout>
+  );
+}
