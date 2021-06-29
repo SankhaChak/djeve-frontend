c58a498ebf19c77e14ae09c9073f40c27365a139
SankhaChak <anonproductionspro@gmail.com>
2021-06-29 17:15:25 +0530

    ✅ feat: added component EventItem

---
+++ components/EventItem.js
@@ -0,0 +1,33 @@
+import Link from "next/link";
+import Image from "next/image";
+import styles from "@/styles/EventItem.module.css";
+
+export default function EventItem({ event }) {
+  return (
+    <div className={styles.event}>
+      <div className={styles.img}>
+        <Image
+          alt="Event Detail"
+          src={
+            event.image
+              ? event.image.formats.thumbnail.url
+              : "/images/event-default.png"
+          }
+          width={170}
+          height={100}
+        />
+      </div>
+      <div className={styles.info}>
+        <span>
+          {new Date(event.date).toLocaleDateString("en-US")} at {event.time}
+        </span>
+        <h3>{event.name}</h3>
+      </div>
+      <div className={styles.link}>
+        <Link href={`/events/${event.slug}`}>
+          <a className="btn">Details</a>
+        </Link>
+      </div>
+    </div>
+  );
+}
