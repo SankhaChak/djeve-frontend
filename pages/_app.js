83e78110f3ccc5cadda8cf705052a8cdc53bbc78
SankhaChak <anonproductionspro@gmail.com>
2021-07-03 15:16:31 +0530

    ✅ feat: route change indicator

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,12 @@
 import { AuthProvider } from "@/context/AuthContext";
 import "../styles/globals.css";
+import Router from "next/router";
+import NProgress from "nprogress";
+import "nprogress/nprogress.css";
+
+Router.events.on("routeChangeStart", () => NProgress.start());
+Router.events.on("routeChangeComplete", () => NProgress.done());
+Router.events.on("routeChangeError", () => NProgress.done());
 
 function MyApp({ Component, pageProps }) {
   return (
